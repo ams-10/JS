8c93ba3c9f724d2418e5188ae8cee88bd827afe0
ams-10 <abdullahshareef123h@gmail.com>
2023-05-30 08:14:11 -0200

    to merge

--- jsLoops.js
+++ jsLoops.js
@@ -1,4 +1,4 @@
 //loops are used to execute a single or multiple statements any number of times.
 //loops increase the time complexity it takes to execute a program
 
-for lop
+//for loops are used to execute a single or multiple statements any number of times
