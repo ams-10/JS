ce47add26a7bde37a4e3fab196feb9f469883b82
ams-10 <abdullahshareef123h@gmail.com>
2023-05-30 08:31:43 -0200

    added for loop statement explanation and syntax

--- jsLoops.js
+++ jsLoops.js
@@ -2,3 +2,11 @@
 //loops increase the time complexity it takes to execute a program
 
 //for loops are used to execute a single or multiple statements any number of times
+//a for loop has three expressions
+//1.an expression during the start 
+//2.an expression for the condition
+//3.an expression for the end
+
+for (let i = 1; i <=5 ; i++) {
+     console.log("Hello World");
+}
