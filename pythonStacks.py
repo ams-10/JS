2904c226aa8f58a2e05d8e0a80aba8b757d9fc26
ams-10 <abdullahshareef123h@gmail.com>
2023-05-30 06:40:46 -0300

    "created the stack"

---
+++ pythonStacks.py
@@ -0,0 +1,16 @@
+class Stack():
+     def __init__(self):
+          self.container = []
+     def push(self, item):
+          self.container.append(item)
+     def pop(self):
+          self.container.pop()
+     def show(self):
+          print(self.container)
+obj = Stack()
+obj.push(1)
+obj.push(2)
+obj.push(3)
+obj.show()
+obj.pop()
+obj.show()
