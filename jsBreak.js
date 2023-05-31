8e7e4549d594687f9436417862817f811ea7bb6b
ams-10 <abdullahshareef123h@gmail.com>
2023-05-31 10:33:31 -0300

    added continue statement and syntax

--- jsBreak.js
+++ jsBreak.js
@@ -6,5 +6,13 @@ for (let x of fruits){
           console.log(x);
      }
 }
+console.log("\n--------------------------------\n");
+
 //when the control of the code encounters mango it breaks and exits out of the loop
 
+//the continue statement is used to skip a single step in the loop
+let myArray = [ "banana", "cherry", "apple", "mango", "grapes", "watermelon"];
+for (let x of myArray){
+     if(x === "mango" || x === "cherry"){continue;}
+     else{console.log(x);}
+}
