c71097775b144c0319a679419fee74cc83c51075
ams-10 <abdullahshareef123h@gmail.com>
2023-05-31 10:52:40 -0300

    added set methods

--- jsSets.js
+++ jsSets.js
@@ -1,4 +1,13 @@
 //js sets are objects which contain a collection of unique values
 //each value can only occur once in a set and it cannot contain any duplicates
 //set is initialized by passing an array to the Set() constructor 
-//example:
+
+/*
+there are five methods to create delete and modify sets 
+they are:
+add() "adds a value to the set"
+delete() "deletes the value from the set"
+has() "returns true if the set contains the specified value"
+forEach() "iterates over the values in the set"
+size() "returns the number of elements in the set"
+*/
