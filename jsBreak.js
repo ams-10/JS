e2dae0777439e94fefada041e1a4305fe571472b
ams-10 <abdullahshareef123h@gmail.com>
2023-05-31 10:29:55 -0300

    Break statement to break loop

---
+++ jsBreak.js
@@ -0,0 +1,10 @@
+//the break statement is used to break the loop
+let fruits = [ "banana", "cherry", "apple", "mango", "grapes", "watermelon"];
+for (let x of fruits){
+     if(x === "mango"){break;}
+     else{
+          console.log(x);
+     }
+}
+//when the control of the code encounters mango it breaks and exits out of the loop
+
