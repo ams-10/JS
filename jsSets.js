1d3d7e4d86a73715232d59c5c8d552a1518a701b
ams-10 <abdullahshareef123h@gmail.com>
2023-05-31 10:41:28 -0300

    added set syntax and explanation

---
+++ jsSets.js
@@ -0,0 +1,4 @@
+//js sets are objects which contain a collection of unique values
+//each value can only occur once in a set and it cannot contain any duplicates
+//set can is enclosed in ([parenthesis followed by a square brackets])
+//example:
