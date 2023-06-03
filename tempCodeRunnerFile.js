5993d470037a3ec8180024069cdff3feeaa51539
ams-10 <abdullahshareef123h@gmail.com>
2023-06-03 13:18:02 -0300

    errors in this program

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,14 +1,20 @@
-//variable scope is the phenomenon of accessing the variable from distinct code blocks.
+h = "javascript";
+// {
+// {
+//      let x = 20
+//      console.log(x);
+//      var y = 30;
+// }
+// let result1 = x + 20;
+// let result2 = x + 30;
+// console.log(result2);
+// console.log(result1);
+// }
 
-//block scope : variables declared with let in a block cannot be accessed from outside the block and vice versa for variable declared with var
-{
-     let x = 20
-     console.log(x);
-     var y = 30;
+//variables declared in a function will have local scope and can only be accessed from within the function
+function hello() {
+     let g = "world";
+     console.log("hello "+h);
+     console.log("hello "+y);
 }
-let result1 = x + 20;
-let result2 = x + 30;
-console.log(result2);
-console.log(result1);
-
-//variables declared in a function will have local scope
+hello();
