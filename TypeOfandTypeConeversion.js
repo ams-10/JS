57032d04cf5657a228e80fb0419e3e9a922ebe62
ams-10 <abdullahshareef123h@gmail.com>
2023-06-03 05:34:13 -0300

    added a new file

---
+++ TypeOfandTypeConeversion.js
@@ -0,0 +1,20 @@
+//there are many types of objects and data types in javaScript
+/*
+in javaScript there are five different dattypes that can contain values
+string
+boolean 
+object 
+function
+
+there are 6 types of objects
+object 
+date 
+array 
+string 
+number
+boolean
+
+and two data types that can contain no values
+Nan
+undefined
+*/
