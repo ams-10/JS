1dc12ff8d2154d1d32c01172056c9cfffb184fe4
ams-10 <abdullahshareef123h@gmail.com>
2023-06-03 16:57:49 -0300

    explanation completed

---
+++ modules.js
@@ -0,0 +1,4 @@
+//modules allow you to break up your code into components and allow you to import classes mehtod functions and variables etc
+//this makes it easier to maintain a code base
+//modules are imported using the import keyword
+//let us export our class file and create a new class of cats with the same attributes and a new attribute age
